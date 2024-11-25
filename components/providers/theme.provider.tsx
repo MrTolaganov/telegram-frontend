42b3fae2ac5ff32a4ecc068e4196eb51f1018517
Otabek <otabektolaganov69@gmail.com>
2024-11-26 01:11:02 +0500

    26/11/2024

--- components/providers/theme.provider.tsx
+++ components/providers/theme.provider.tsx
@@ -2,15 +2,14 @@
 
 import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import NoSSR from 'react-no-ssr'
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return (
-    <NextThemesProvider {...props}>
-      <NoSSR>{children}</NoSSR>
-    </NextThemesProvider>
-  )
+  const [mounted, setMounted] = React.useState(false)
+
+  React.useEffect(() => setMounted(true), [])
+
+  return <NextThemesProvider {...props}>{mounted && children}</NextThemesProvider>
 }
