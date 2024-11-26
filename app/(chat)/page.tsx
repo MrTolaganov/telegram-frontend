9b203e56f7b3458e8da14658a50f0227d9d556c1
Otabek <otabektolaganov69@gmail.com>
2024-11-26 10:32:27 +0500

    26/11/2024 10:32

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -289,7 +289,7 @@ export default function Page() {
   }
 
   useEffect(() => {
-    socketRef.current = io(process.env.WEBSOCKET_URI, { transports: ['websocket'] })
+    socketRef.current = io('wss://telegram-websocket.onrender.com')
   }, [])
 
   useEffect(() => {
